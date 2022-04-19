e384a5efe122a642b296af86e3d0c5a24b487969
AstroJackson <jacksondtaylor4@gmail.com>
2022-04-19 16:52:05 -0400

    update

--- Lab 8/wave_diff_equation.py
+++ Lab 8/wave_diff_equation.py
@@ -4,6 +4,13 @@ from scipy.integrate import quad as integrate
 from matplotlib.animation import FuncAnimation
 import matplotlib.patches as mpl_patches
 
+"""
+See "" to know where to edit phi and psi, or whether to use the wave equation or the diffusion equation.
+Phi can be found on line 37
+Psi on line 45
+Wave or diff eq on line 71
+"""
+
 a = 1.
 c = 1.
 k = .1
@@ -30,13 +37,15 @@ def order3(x):
     return 15 * (x**3-1.42*x**2 + .5*x)
 
 def phi_sin(x, n, *args):
-    phi = order3(x)
-    return phi * np.sin(n*pi*x/a)
+    phi = flat_then_peak(x) #initial condition
+    """^^This can be edited. The above functions are just examples I tested."""  
+    return phi * np.sin(n*pi*x/a) #function to be integrated over to determine the FS coefficient
 
 #plt.plot(np.linspace(0, a, points), gaussian(np.linspace(0, a, points), s))
 
 def psi_sin(x, n, *args):
     psi = 0
+    """^^This can be edited. The above functions are just examples I tested."""  
     return psi * np.sin(n*pi*x/a)
 
 def An(n, *args):
@@ -59,6 +68,7 @@ total_frames = int(max_time/interval)
 times = np.arange(0, max_time, interval)
 u = [0 for i in range(len(times))]
 
+"""Switch one to True, the other to False."""
 wave_equation = False
 dif_equation = True
 if wave_equation:
